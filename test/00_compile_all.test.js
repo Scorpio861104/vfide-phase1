63aa8095c9c89d3e89831429402ed11f26fcfebc
Scorpio861104 <markebutters@gmail.com>
2025-09-13 00:39:57 -0700

    Update 00_compile_all.test.js

--- test/00_compile_all.test.js
+++ test/00_compile_all.test.js
@@ -1,1 +1,18 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
+const CONTRACTS = [
+  "contracts/phase1/VFIDEToken.sol:VFIDEToken",
+  "contracts/phase1/VFIDEPresale.sol:VFIDEPresale",
+  "contracts/phase1/ProofLedger.sol:ProofLedger",
+  "contracts/phase1/SanctumFund.sol:SanctumFund"
+];
+
+describe("Compile all Phase-1 contracts (no deploy)", function () {
+  for (const fqn of CONTRACTS) {
+    it(`loads factory for ${fqn}`, async function () {
+      const F = await ethers.getContractFactory(fqn);
+      expect(F.interface).to.exist;
+    });
+  }
+});
