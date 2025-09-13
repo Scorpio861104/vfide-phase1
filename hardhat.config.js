8224bacd7981ba69782c6e6374395cb11021b848
Scorpio861104 <markebutters@gmail.com>
2025-09-13 00:37:25 -0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,9 @@
+require("@nomicfoundation/hardhat-toolbox");
 
+module.exports = {
+  solidity: {
+    version: "0.8.30",
+    settings: { optimizer: { enabled: true, runs: 200 }, evmVersion: "paris" }
+  },
+  mocha: { timeout: 120000 }
+};
